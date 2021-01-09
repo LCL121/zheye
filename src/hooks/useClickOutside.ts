cc09db37539790a1af16d67b7655e954ee706a6d
LCL121 <1391436522@qq.com>
2021-01-09 23:07:51 +0800

    完成Dropdown 组件

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,23 @@
+import { ref, onMounted, onUnmounted, Ref } from 'vue'
+
+const useClickOutside = (elementRef: Ref<null | HTMLElement>) => {
+  const isClickOutside = ref(false)
+  const handler = (e: MouseEvent) => {
+    if (elementRef.value) {
+      if (!elementRef.value.contains(e.target as HTMLElement)) {
+        isClickOutside.value = true
+      } else {
+        isClickOutside.value = false
+      }
+    }
+  }
+  onMounted(() => {
+    document.addEventListener('click', handler)
+  })
+  onUnmounted(() => {
+    document.removeEventListener('click', handler)
+  })
+  return isClickOutside
+}
+
+export default useClickOutside
