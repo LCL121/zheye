f76e10a60b44866be24400de3de728211d134ce7
LCL121 <1391436522@qq.com>
2021-01-09 21:17:38 +0800

    完成columnList 组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介，可以更新一下哦'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test3的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test4的专栏，有一段非常有意思的简介，可以更新一下哦',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'development') {
+      config.module
+        .rule('eslint')
+        .use('eslint-loader')
+        .loader('eslint-loader')
+        .tap(options => {
+          options.fix = true
+          return options
+        })
+    }
+  }
+}
