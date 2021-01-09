005b46ec52529a25006d95c4e6bb2bd31a891a2d
LCL121 <1391436522@qq.com>
2021-01-09 21:35:48 +0800

    完成GlobalHeader 组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,6 +9,7 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
 
 const testData: ColumnProps[] = [
   {
@@ -36,14 +38,21 @@ const testData: ColumnProps[] = [
   }
 ]
 
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'lcl'
+}
+
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
