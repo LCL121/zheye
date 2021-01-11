0dbbff1cd9e8292acc9be944d03033db8590fabc
LCL121 <1391436522@qq.com>
2021-01-11 13:40:14 +0800

    完成 ValidateInput组件

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,43 @@
   <div class="container">
     <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
+    <form>
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input
+          type="text"
+          :rules="emailRules"
+          v-model="emailVal"
+          placeholder="请输入正确的邮箱地址"
+        ></validate-input>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">密码</label>
+        <validate-input
+          type="password"
+          :rules="passwordRules"
+          v-model="passwordVal"
+          placeholder="请输入密码"
+        ></validate-input>
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
 
 const testData: ColumnProps[] = [
   {
     id: 1,
     title: 'test1的专栏',
     description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下哦',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
   },
   {
     id: 2,
@@ -28,13 +50,15 @@ const testData: ColumnProps[] = [
     id: 3,
     title: 'test3的专栏',
     description: '这是test3的专栏，有一段非常有意思的简介，可以更新一下哦',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
   },
   {
     id: 4,
     title: 'test4的专栏',
     description: '这是test4的专栏，有一段非常有意思的简介，可以更新一下哦',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5f3e41a8b7d9c60b68cdd1ec.jpg'
   }
 ]
 
@@ -43,16 +67,32 @@ const currentUser: UserProps = {
   name: 'lcl'
 }
 
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
 export default defineComponent({
   name: 'App',
   components: {
     ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
-  setup () {
+  setup() {
+    const emailVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const passwordVal = ref('')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' }
+    ]
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRules,
+      emailVal,
+      passwordRules,
+      passwordVal
     }
   }
 })
